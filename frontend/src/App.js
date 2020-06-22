9a9921976617d90c8434ea1cbd9dc456caa0a1ca
fabianschmauder <fabianschmauder@gmail.com>
2020-06-22 12:50:49 +0200

    Fetch all ideas

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div>
-    </div>
-  );
+
+    const [ideas, setIdeas] = useState([]);
+
+    useEffect(() => {
+        fetch('/api/ideas')
+            .then(response => response.json())
+            .then(data => setIdeas(data))
+    }, []);
+
+
+    return (
+        <div>
+            {ideas.map((idea) =>
+                <div key={idea.id}>
+                    {idea.description}
+                </div>)}
+        </div>
+    );
 }
 
 export default App;
