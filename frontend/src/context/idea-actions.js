7065113c478e5995b55c7e7c5f8182eacc768f2f
Rolf Buchwald Alvarez <rolf_buchwald@gmx.net>
2020-06-29 14:46:48 +0200

    Add wip

--- frontend/src/context/idea-actions.js
+++ frontend/src/context/idea-actions.js
@@ -3,6 +3,9 @@ import {fetchAllIdeas} from "../utils/ideas-utils";
 export const FETCH_IDEAS = 'FETCH_IDEAS'
 export const FETCH_IDEAS_SUCCESS = 'FETCH_IDEAS_SUCCESS'
 export const FETCH_IDEAS_FAILED = 'FETCH_IDEAS_FAILED'
+export const ADD_IDEA = 'ADD_IDEA'
+export const ADD_IDEA_SUCCESS = 'ADD_IDEA_SUCCESS'
+export const ADD_IDEA_FAILED = 'ADD_IDEA_FAILED'
 
 export async function fetchIdeas(dispatch) {
     dispatch({type: FETCH_IDEAS})
@@ -11,5 +14,15 @@ export async function fetchIdeas(dispatch) {
         dispatch({type: FETCH_IDEAS_SUCCESS, payload: ideas})
     } catch (error) {
         dispatch({type: FETCH_IDEAS_FAILED, payload: error})
+    }
+}
+
+export async function addIdea(dispatch) {
+    dispatch({type: ADD_IDEA})
+    try {
+        const idea = await addIdea();
+        dispatch({type: ADD_IDEA_SUCCESS, payload: idea})
+    } catch (error) {
+        dispatch({type: ADD_IDEA_FAILED, payload: error})
     }
 }
--- frontend/src/context/ideaReducer.js
+++ frontend/src/context/ideaReducer.js
@@ -1,6 +1,7 @@
 import {FETCH_IDEAS, FETCH_IDEAS_FAILED, FETCH_IDEAS_SUCCESS} from "./idea-actions";
 
 export default function ideaReducer(state, action) {
+    console.log(action);
     switch (action.type) {
         case FETCH_IDEAS:
             return {...state, fetchStatus: 'PENDING'}
