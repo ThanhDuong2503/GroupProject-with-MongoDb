4e711409d45ff118c088e160a5f67d7eee1430cc
fabianschmauder <fabianschmauder@gmail.com>
2020-06-29 10:35:00 +0200

    Add react router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,37 +1,20 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import './App.css';
-import {fetchAllIdeas} from "./utils/ideas-utils";
-import IdeaCard from "./components/IdeaCard/IdeaCard";
-import Button from "@material-ui/core/Button";
-import AddIdeaDialog from "./components/AddIdeaDialog/AddIdeaDialog";
+import {
+    BrowserRouter,
+    Switch,
+    Route
+} from "react-router-dom";
+import IdeaOverview from "./pages/IdeaOverview";
 
 function App() {
-
-    const [ideas, setIdeas] = useState([]);
-    const [showAddDialog, setShowAddDialog] = useState(false);
-
-
-    useEffect(() => {
-        fetchAllIdeas().then(data => setIdeas(data))
-    }, []);
-
-
-    return (
-        <div className={"app"}>
-            <Button variant="outlined" color="primary" onClick={() => setShowAddDialog(true)}>
-                Add Idea
-            </Button>
-
-            <AddIdeaDialog open={showAddDialog}
-                           handleClose={() => setShowAddDialog(false)}
-                           onAdd={(idea) => setIdeas([...ideas, idea])}
-            />
-            {ideas.map((idea) => <IdeaCard key={idea.id} idea={idea} onDeleteSuccess={() => {
-                const filteredList = ideas.filter(filterIdea => filterIdea.id !== idea.id);
-                setIdeas(filteredList)
-            }}/>)}
-        </div>
-    );
+    return <BrowserRouter>
+        <Switch>
+            <Route path="/">
+                <IdeaOverview/>
+            </Route>
+        </Switch>
+    </BrowserRouter>
 }
 
 export default App;
