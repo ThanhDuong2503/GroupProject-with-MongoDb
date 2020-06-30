e5ae293e51ab38e28091a6210bc35d5d191f4701
fabianschmauder <fabianschmauder@gmail.com>
2020-06-30 15:54:21 +0200

    Init spring security

---
+++ project-planning/src/main/java/de/neuefische/projectplanning/model/PlanningUser.java
@@ -0,0 +1,19 @@
+package de.neuefische.projectplanning.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(collection = "user")
+public class PlanningUser {
+
+  @Id
+  private String username;
+  private String password;
+}
